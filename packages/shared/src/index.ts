3039f491a32938b0feb1d4dbe18d0056d77dc394
R_h_zero <renhongze@able-elec.com>
2022-10-31 16:08:05 +0800

    feat(shared):  add until

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,5 @@
-export const add = () => {
-  return 1 + 1
-}
+export const isObject = val => val !== null && typeof val === 'object'
+
+export const isString = val => typeof val === 'string'
+
+export const extend = Object.assign
